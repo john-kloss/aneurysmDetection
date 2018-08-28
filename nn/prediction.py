7fa30e010c796c7fa418343708340a862552ad13
ulie <ulrike.schnaithmann@gmail.com>
2018-08-28 13:16:45 +0200

    testset

--- nn/prediction.py
+++ nn/prediction.py
@@ -4,26 +4,22 @@ import tables
 from .model import unet_model_3d
 from .generator import DataGenerator, PredictGenerator 
 from .metrics import dice_coefficient
+path = os.getcwd() + "/data/processed/test/" 
 
 def predict():
-    
-    # initialize test data generator without labels for predicting 
-    prediction_data = PredictGenerator([0])
+
     net = unet_model_3d((1,64,64,64))
     net.load_weights("./data/logs/network_weights.h5")
+    
+    for filename in os.listdir(path):
+        f = h5py.File( path + filename , 'r')
+        prediction_data =  f['images/images']    
 
-    predictions = net.predict_generator(prediction_data, verbose=1)   
+
+    predictions = net.predict(prediction_data, verbose=1)   
     
     for pred in predictions:
         dice_coefficient(truth,pred)
 
     
     
-
-def overlap(pred):
-    # calculates overlap of pre
-    test_data = DataGenerator([0])
-    net = unet_model_3d((1,64,64,64))
-    net.load_weights("./data/logs/network_weights.h5")
-
-    return net.evaluate_generator(test_data, verbose=1)
