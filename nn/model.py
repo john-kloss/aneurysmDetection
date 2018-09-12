973f796f210c69ebffff3d0c62e888106baf5bd7
john-kloss <jonathan.kloss1@gmail.com>
2018-09-12 16:24:25 +0200

    updates

--- nn/model.py
+++ nn/model.py
@@ -1,11 +1,24 @@
 import numpy as np
 from keras import backend as K
 from keras.engine import Input, Model
-from keras.layers import Conv3D, MaxPooling3D, UpSampling3D, Activation, BatchNormalization, PReLU, Deconvolution3D
+from keras.layers import (
+    Conv3D,
+    MaxPooling3D,
+    UpSampling3D,
+    Activation,
+    BatchNormalization,
+    PReLU,
+    Deconvolution3D,
+)
 from keras.optimizers import Adam
 from keras.initializers import RandomNormal
-from keras import metrics 
-from .metrics import dice_coefficient_loss, dice_coefficient, create_weighted_binary_crossentropy
+from keras import metrics
+from keras import regularizers
+from .metrics import (
+    dice_coefficient_loss,
+    dice_coefficient,
+    create_weighted_binary_crossentropy,
+)
 
 K.set_image_data_format("channels_first")
 
@@ -15,10 +28,19 @@ except ImportError:
     from keras.layers.merge import concatenate
 
 
-
-def unet_model_3d(input_shape, pool_size=(4,4,4), n_labels=1, initial_learning_rate=0.00001, deconvolution=False,
-                  depth=3, n_base_filters=32, include_label_wise_dice_coefficients=False, metrics=dice_coefficient,
-                  batch_normalization=False, activation_name="sigmoid"):
+def unet_model_3d(
+    input_shape,
+    pool_size=(4, 4, 4),
+    n_labels=1,
+    initial_learning_rate=0.00001,
+    deconvolution=False,
+    depth=3,
+    n_base_filters=32,
+    include_label_wise_dice_coefficients=False,
+    metrics=dice_coefficient,
+    batch_normalization=False,
+    activation_name="sigmoid",
+):
     """
     Builds the 3D UNet Keras model.f
     :param metrics: List metrics to be calculated during model training (default is dice coefficient).
@@ -44,10 +66,16 @@ def unet_model_3d(input_shape, pool_size=(4,4,4), n_labels=1, initial_learning_r
 
     # add levels with max pooling
     for layer_depth in range(depth):
-        layer1 = create_convolution_block(input_layer=current_layer, n_filters=n_base_filters*(2**layer_depth),
-                                          batch_normalization=batch_normalization)
-        layer2 = create_convolution_block(input_layer=layer1, n_filters=n_base_filters*(2**layer_depth)*2,
-                                          batch_normalization=batch_normalization)
+        layer1 = create_convolution_block(
+            input_layer=current_layer,
+            n_filters=n_base_filters * (2 ** layer_depth),
+            batch_normalization=batch_normalization,
+        )
+        layer2 = create_convolution_block(
+            input_layer=layer1,
+            n_filters=n_base_filters * (2 ** layer_depth) * 2,
+            batch_normalization=batch_normalization,
+        )
         if layer_depth < depth - 1:
             current_layer = MaxPooling3D(pool_size=pool_size)(layer2)
             levels.append([layer1, layer2, current_layer])
@@ -56,26 +84,47 @@ def unet_model_3d(input_shape, pool_size=(4,4,4), n_labels=1, initial_learning_r
             levels.append([layer1, layer2])
 
     # add levels with up-convolution or up-sampling
-    for layer_depth in range(depth-2, -1, -1):
-        up_convolution = get_up_convolution(pool_size=pool_size, deconvolution=deconvolution,
-                                            n_filters=current_layer._keras_shape[1])(current_layer)
+    for layer_depth in range(depth - 2, -1, -1):
+        up_convolution = get_up_convolution(
+            pool_size=pool_size,
+            deconvolution=deconvolution,
+            n_filters=current_layer._keras_shape[1],
+        )(current_layer)
         concat = concatenate([up_convolution, levels[layer_depth][1]], axis=1)
-        current_layer = create_convolution_block(n_filters=levels[layer_depth][1]._keras_shape[1],
-                                                 input_layer=concat, batch_normalization=batch_normalization)
-        current_layer = create_convolution_block(n_filters=levels[layer_depth][1]._keras_shape[1],
-                                                 input_layer=current_layer,
-                                                 batch_normalization=batch_normalization)
+        current_layer = create_convolution_block(
+            n_filters=levels[layer_depth][1]._keras_shape[1],
+            input_layer=concat,
+            batch_normalization=batch_normalization,
+        )
+        current_layer = create_convolution_block(
+            n_filters=levels[layer_depth][1]._keras_shape[1],
+            input_layer=current_layer,
+            batch_normalization=batch_normalization,
+        )
 
     final_convolution = Conv3D(n_labels, (1, 1, 1))(current_layer)
     act = Activation(activation_name)(final_convolution)
     model = Model(inputs=inputs, outputs=act)
 
-    model.compile(optimizer=Adam(lr=initial_learning_rate), loss=create_weighted_binary_crossentropy(0.05,0.95), metrics=['binary_crossentropy', dice_coefficient], sample_weight_mode='temporal')
+    model.compile(
+        optimizer=Adam(lr=initial_learning_rate),
+        loss=create_weighted_binary_crossentropy(0.05, 0.95),
+        metrics=["binary_crossentropy", dice_coefficient],
+        sample_weight_mode="temporal",
+    )
     return model
 
 
-def create_convolution_block(input_layer, n_filters, batch_normalization=False, kernel=(3,3,3), activation=None,
-                             padding='same', strides=(1, 1, 1), instance_normalization=False):
+def create_convolution_block(
+    input_layer,
+    n_filters,
+    batch_normalization=False,
+    kernel=(3, 3, 3),
+    activation=None,
+    padding="same",
+    strides=(1, 1, 1),
+    instance_normalization=False,
+):
     """
     :param strides:
     :param input_layer:
@@ -87,18 +136,27 @@ def create_convolution_block(input_layer, n_filters, batch_normalization=False, 
     :return:
     """
     init = RandomNormal(mean=0.0, stddev=0.05, seed=None)
-    layer = Conv3D(n_filters, kernel, padding=padding, strides=strides, bias_initializer=init)(input_layer)
+    layer = Conv3D(
+        n_filters,
+        kernel,
+        padding=padding,
+        strides=strides,
+        bias_initializer=init,
+        kernel_regularizer=regularizers.l2(0.01),
+    )(input_layer)
     if batch_normalization:
         layer = BatchNormalization(axis=1)(layer)
     elif instance_normalization:
         try:
             from keras_contrib.layers.normalization import InstanceNormalization
         except ImportError:
-            raise ImportError("Install keras_contrib in order to use instance normalization."
-                              "\nTry: pip install git+https://www.github.com/farizrahman4u/keras-contrib.git")
+            raise ImportError(
+                "Install keras_contrib in order to use instance normalization."
+                "\nTry: pip install git+https://www.github.com/farizrahman4u/keras-contrib.git"
+            )
         layer = InstanceNormalization(axis=1)(layer)
     if activation is None:
-        return Activation('relu')(layer)
+        return Activation("relu")(layer)
     else:
         return activation()(layer)
 
@@ -113,14 +171,18 @@ def compute_level_output_shape(n_filters, depth, pool_size, image_shape):
     :param depth: The number of levels down in the U-shaped model a given node is.
     :return: 5D vector of the shape of the output node 
     """
-    output_image_shape = np.asarray(np.divide(image_shape, np.power(pool_size, depth)), dtype=np.int32).tolist()
+    output_image_shape = np.asarray(
+        np.divide(image_shape, np.power(pool_size, depth)), dtype=np.int32
+    ).tolist()
     return tuple([None, n_filters] + output_image_shape)
 
 
-def get_up_convolution(n_filters, pool_size, kernel_size=(2, 2, 2), strides=(2, 2, 2),
-                       deconvolution=False):
+def get_up_convolution(
+    n_filters, pool_size, kernel_size=(2, 2, 2), strides=(2, 2, 2), deconvolution=False
+):
     if deconvolution:
-        return Deconvolution3D(filters=n_filters, kernel_size=kernel_size,
-                               strides=strides)
+        return Deconvolution3D(
+            filters=n_filters, kernel_size=kernel_size, strides=strides
+        )
     else:
         return UpSampling3D(size=pool_size)
